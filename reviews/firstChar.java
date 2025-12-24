6ebf5c1c47ce5de0cf7d47de27dbbcad5a44d4f0
Manish Pailwar <ram823890@gmail.com>
2025-12-24 11:38:31 +0530

    [Manish Pailwar] created new branch REVIEWS

---
+++ reviews/firstChar.java
@@ -0,0 +1,25 @@
+package reviews;
+import java.util.*;
+public class firstChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s = sc.next();
+        boolean b = true ;
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<s.length();j++){
+                if(s.charAt(i)!=s.charAt(j)){
+                    b = true ;
+            }
+            else {
+                b = false ;
+                break ;
+            }
+     }
+     if(b){
+        System.out.println(s.charAt(i));
+        break ;
+     }
+        }
+        sc.close();
+    }
+}
