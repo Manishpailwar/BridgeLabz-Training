66dca3a46e9612a7018a9df058a97fa47b903cd8
Manish Pailwar <ram823890@gmail.com>
2026-01-05 23:39:47 +0530

    [Manish Pailwar] Solved Leetcode Problem

---
+++ oops-practice/leetcode/ReverseInteger.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Solution {
+    public int reverse(int x) {
+        long rem = 0;
+        if(x < 0){
+        x = Math.abs(x);
+         while(x>0){
+            rem = rem*10 + x%10 ;
+            if(rem < -2147483648 || rem > 2147483647)
+             return 0;
+            x = x/10 ;
+        }
+        rem = -rem;
+        int n = (int)rem;
+        return n;
+        }
+        else{
+        while(x>0){
+            rem = rem*10 + x%10 ;
+            if(rem < -2147483648 || rem > 2147483647)
+            return 0;
+            x = x/10 ;
+        }
+        int n = (int)rem;
+        return n;
+        }
+    }
+}
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        Solution solution = new Solution();
+        sc.close();
+        System.out.println(solution.reverse(n));
+    }
+}
